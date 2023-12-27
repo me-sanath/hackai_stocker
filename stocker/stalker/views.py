e8f486f7bc278c5d192d0cc955d191cb0e9a9a48
Satwik Kini <satwikkini01@gmail.com>
2023-12-27 18:50:42 +0530

    added stock function to views.py

--- stocker/stalker/views.py
+++ stocker/stalker/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from getStockPrice import getStockPrice
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+
+class getStock(APIView):
+    def get(self,request):
+        stock_symbol = request.GET.get('stock_symbol')
+        response = getStockPrice(stock_symbol)
+        message = {
+            'message':response
+        }
+        print(message)
+        return JsonResponse(message)
